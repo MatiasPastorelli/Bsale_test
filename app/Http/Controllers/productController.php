27238386c298bfa7c1b0732d4f0020fe81d331dc
Matias <m.pharmatender.cl>
2021-04-26 23:21:19 -0400

    heroku

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Validator;
 use DB;
 
-class productController extends Controller
+class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
